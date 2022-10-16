3632754705d62e708f96307174a9b1355c13df6c
Miles Dame <milesedame@gmail.com>
2022-10-16 15:08:23 -0400

    Add accept method

--- src/input/components/ComponentNode.java
+++ src/input/components/ComponentNode.java
@@ -1,6 +1,11 @@
 package input.components;
+
+import input.visitor.ComponentNodeVisitor;
+
 // Test
 public interface ComponentNode
 {
 	void unparse(StringBuilder sb, int level);
+	
+	Object accept(ComponentNodeVisitor visitor, Object o);
 }
