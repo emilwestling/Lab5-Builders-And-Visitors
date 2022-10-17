39e62582c3efd185c9a9129ea6cf36404308ae4c
Icho3159 <105511673+Icho3159@users.noreply.github.com>
2022-10-17 19:49:10 -0400

    fix made to visitPointNode
    .

--- src/input/visitor/UnparseVisitor.java
+++ src/input/visitor/UnparseVisitor.java
@@ -121,13 +121,15 @@ public class UnparseVisitor implements ComponentNodeVisitor
 	@Override
 	public Object visitPointNode(PointNode node, Object o)
 	{
+			@SuppressWarnings("unchecked")
 			AbstractMap.SimpleEntry<StringBuilder, Integer> pair = (AbstractMap.SimpleEntry<StringBuilder, Integer>)(o);
 			StringBuilder sb = pair.getKey();
 			int level = pair.getValue();
 			
 			sb.append(StringUtilities.indent(level + 1));
-			sb.append("Point(" + node.getName() + ")" + "(" + String.valueOf(p.getX()) + ")"
+			sb.append("Point(" + node.getName() + ")" + "(" + String.valueOf(node.getX()) + ")"
 			+ "(" + String.valueOf(node.getY()) + ") \n");
+			return sb;
 		
 	}
 }
